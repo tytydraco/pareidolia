efed110830e1114f8c6025050d2964a1c103a784
Tyler Nijmeh <tylernij@gmail.com>
2025-02-07 20:16:20 -0800

    Build main features
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- example/pareidolia_example.dart
+++ example/pareidolia_example.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
+import 'package:image/image.dart';
 import 'package:pareidolia/pareidolia.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final pareidolia = Pareidolia(seed: 12345);
+  final image = pareidolia.generate(
+    width: 1024,
+    height: 512,
+    redMax: 0,
+    blueMax: 0,
+  );
+  final png = encodePng(image);
+  File('image.png').writeAsBytesSync(png);
 }
--- lib/src/pareidolia_base.dart
+++ lib/src/pareidolia_base.dart
@@ -1,6 +1,44 @@
-// TODO: Put public facing types in this file.
+import 'dart:math';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:image/image.dart';
+
+/// Random image generator.
+class Pareidolia {
+  /// Creates a new [Pareidolia].
+  Pareidolia({
+    this.seed,
+  });
+
+  /// The random seed for the random number generator.
+  final int? seed;
+
+  /// The random number generator instance.
+  late final random = Random(seed);
+
+  int _randomRange(int min, int max) {
+    return random.nextInt(max - min + 1) + min;
+  }
+
+  /// Generates an image with random color data.
+  Image generate({
+    required int width,
+    required int height,
+    int redMin = 0,
+    int redMax = 255,
+    int greenMin = 0,
+    int greenMax = 255,
+    int blueMin = 0,
+    int blueMax = 255,
+  }) {
+    final image = Image(width: width, height: height);
+
+    for (final pixel in image) {
+      pixel
+        ..r = _randomRange(redMin, redMax)
+        ..g = _randomRange(greenMin, greenMax)
+        ..b = _randomRange(blueMin, blueMax);
+    }
+
+    return image;
+  }
 }
